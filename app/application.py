005f88110a3d4cb34eaf4e921973db9bcf2fe0f1
svetlannnka <svetlana.levinsohn@gmail.com>
2025-03-27 20:08:48 -0700

    Lesson 8

--- app/application.py
+++ app/application.py
@@ -3,6 +3,7 @@ from pages.cart_page import CartPage
 from pages.header import Header
 from pages.main_page import MainPage
 from pages.search_results_page import SearchResultsPage
+from pages.target_app_page import TargetAppPage
 
 
 class Application:
@@ -10,7 +11,9 @@ class Application:
     def __init__(self, driver):
         self.driver = driver
 
+        self.base_page = Page(driver)
         self.cart_page = CartPage(driver)
         self.header = Header(driver)
         self.main_page = MainPage(driver)
         self.search_results_page = SearchResultsPage(driver)
+        self.target_app_page = TargetAppPage(driver)
